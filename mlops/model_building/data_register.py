68945b6ff3745f5a7b06883b9b24b542e8c3da72
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-17 11:31:58 +0530

    Update data_register.py

--- mlops/model_building/data_register.py
+++ mlops/model_building/data_register.py
@@ -1,15 +1,24 @@
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
 
-create_repo(
-    "bank-customer-churn",  # Your dataset repo name
-    repo_type="dataset",  # Specify this is a dataset
-    private=False,  # Set to True if it should be private
-)
+
+repo_id = "praneeth232/bank-customer-churn"
+repo_type = "space"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, space_sdk=space_sdk)
+    print(f"Space '{repo_id}' created.")
+
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="mlops/data",
-    repo_id="praneeth232/bank-customer-churn",
+    repo_id=repo_id,
     repo_type="dataset",
 )
