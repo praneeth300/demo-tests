e7c612f262a97591f5659896032ea0627489d5c8
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-17 11:34:40 +0530

    Update train.py

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -103,10 +103,22 @@ with mlflow.start_run():
 joblib.dump(best_model, "best_churn_model.joblib")
 
 # Upload to Hugging Face
-create_repo("churn-model", repo_type="model", private=False)
+repo_id = "praneeth232/churn-model"
+repo_type = "model"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
+# create_repo("churn-model", repo_type="model", private=False)
 api.upload_file(
     path_or_fileobj="best_churn_model.joblib",
     path_in_repo="best_churn_model.joblib",
-    repo_id="praneeth232/churn-model",
-    repo_type="model",
+    repo_id=repo_id,
+    repo_type=repo_type,
 )
