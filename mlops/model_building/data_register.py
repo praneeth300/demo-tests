16dc0c79d52c397414b32e8c59e6dad7fe1f39b0
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-17 11:35:57 +0530

    Update data_register.py

--- mlops/model_building/data_register.py
+++ mlops/model_building/data_register.py
@@ -12,7 +12,7 @@ try:
     print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type)
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
 
 
