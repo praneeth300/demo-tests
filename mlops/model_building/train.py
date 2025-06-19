80263d2296856272485f34b82939c405f6fbee17
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-19 17:43:27 +0530

    Update train.py

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -110,23 +110,23 @@ with mlflow.start_run():
     mlflow.log_artifact(model_path, artifact_path="model")
     print(f"Model saved as artifact at: {model_path}")
 
-# Upload to Hugging Face
-repo_id = "praneeth232/churn-model"
-repo_type = "model"
-
-# Step 1: Check if the space exists
-try:
-    api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
-except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
-
-# create_repo("churn-model", repo_type="model", private=False)
-api.upload_file(
-    path_or_fileobj="best_churn_model.joblib",
-    path_in_repo="best_churn_model.joblib",
-    repo_id=repo_id,
-    repo_type=repo_type,
-)
+    # Upload to Hugging Face
+    repo_id = "praneeth232/churn-model"
+    repo_type = "model"
+    
+    # Step 1: Check if the space exists
+    try:
+        api.repo_info(repo_id=repo_id, repo_type=repo_type)
+        print(f"Space '{repo_id}' already exists. Using it.")
+    except RepositoryNotFoundError:
+        print(f"Space '{repo_id}' not found. Creating new space...")
+        create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+        print(f"Space '{repo_id}' created.")
+    
+    # create_repo("churn-model", repo_type="model", private=False)
+    api.upload_file(
+        path_or_fileobj="best_churn_model.joblib",
+        path_in_repo="best_churn_model.joblib",
+        repo_id=repo_id,
+        repo_type=repo_type,
+    )
