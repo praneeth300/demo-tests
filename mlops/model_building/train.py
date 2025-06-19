caf15e7373fe90a49c27b2c4f3939c6f6e082372
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-19 11:49:20 +0530

    Update train.py

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -18,7 +18,7 @@ os.environ['MLFLOW_TRACKING_USERNAME'] = "praneeth300"
 os.environ['MLFLOW_TRACKING_PASSWORD'] = "ad3241e355dba7ea2535b74028c14c97190dac88"
 
 mlflow.set_tracking_uri("https://dagshub.com/praneeth300/my-first-repo.mlflow/")
-mlflow.set_experiment("mlops-training-experiment")
+mlflow.set_experiment("mlops-training-experiment-3")
 
 # Hugging Face API authentication
 api = HfApi(token=os.getenv("HF_TOKEN"))
